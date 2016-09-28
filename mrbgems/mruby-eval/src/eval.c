a7741ef69946cf937ea45b578e0a273ae111e30d
Nozomi SATO <joypeff@gmail.com>
2016-09-28 22:26:42 +0900

    implement write-back lvs. all test have been passed.

--- mrbgems/mruby-eval/src/eval.c
+++ mrbgems/mruby-eval/src/eval.c
@@ -4,42 +4,69 @@
 #include <mruby/irep.h>
 #include <mruby/proc.h>
 #include <mruby/opcode.h>
-#include <stdbool.h>
+
+static mrb_irep *
+get_closure_irep(mrb_state *mrb, struct REnv *env) {
+  struct RProc *proc;
+  mrb_irep *irep;
+
+  if (!MRB_ENV_STACK_SHARED_P(env)) {
+    /* skip it because non shared env has special cioff */
+    return NULL;
+  }
+
+  proc = mrb->c->cibase[env->cioff].proc;
+  if (!proc || MRB_PROC_CFUNC_P(proc)) {
+    /* cfunc has neither local variable and related symbol */
+    return NULL;
+  }
+
+  irep = proc->body.irep;
+  if (!irep || irep->nlocals < 1) {
+    return NULL;
+  }
+
+  return irep;
+}
+
+static void
+writeback_lvs(mrb_state *mrb, struct RProc *proc) {
+  struct REnv *env = proc->env;
+  mrb_value *sstack = mrb->c->stack + 1;
+
+  while (env) {
+    mrb_irep *irep = get_closure_irep(mrb, env);
+    if (irep) {
+      int i;
+      for (i = 0; i < irep->nlocals - 1; i++) {
+        env->stack[irep->lv[i].r] = *sstack++;
+      }
+    }
+
+    env = (struct REnv *)env->c;
+  }
+}
 
 static unsigned int
-__capture_lvs(mrb_state *mrb, mrbc_context *cxt, struct REnv *env, bool pretend)
+__capture_lvs(mrb_state *mrb, mrbc_context *cxt, struct REnv *env, mrb_bool pretend)
 {
   mrb_sym *syms = cxt->syms;
   mrb_value *stack = mrb->c->stack + 1; /* All stack[0]s are belong to receiver */
   unsigned int slen = 0;
 
   while (env) {
-    struct RProc *proc;
-    mrb_irep *irep;
-
-    if (!MRB_ENV_STACK_SHARED_P(env)) {
-      break;
-    }
-
-    proc = mrb->c->cibase[env->cioff].proc;
-    if (!proc || MRB_PROC_CFUNC_P(proc)) {
-      break;
-    }
-
-    irep = proc->body.irep;
-    if (!irep || irep->nlocals < 1) {
-      break;
-    }
-
-    if (!pretend && syms) {
-      int i;
-      for (i = 0; i < irep->nlocals - 1; i++) {
-        *syms++ = irep->lv[i].name;
-        *stack++ = env->stack[irep->lv[i].r];
+    mrb_irep *irep = get_closure_irep(mrb, env);
+    if (irep) {
+      if (!pretend && syms) {
+        int i;
+        for (i = 0; i < irep->nlocals - 1; i++) {
+          *syms++ = irep->lv[i].name;
+          *stack++ = env->stack[irep->lv[i].r];
+        }
       }
-    }
 
-    slen += irep->nlocals - 1;
+      slen += irep->nlocals - 1;
+    }
 
     env = (struct REnv *)env->c;
   }
@@ -51,15 +78,22 @@ static unsigned int
 capture_lvs(mrb_state *mrb, mrbc_context *cxt, struct REnv *env)
 {
   unsigned int slen;
-  slen = __capture_lvs(mrb, cxt, env, true) + 1; /* 1 is for receiver */
+  /* just get the required stack length */
+  slen = __capture_lvs(mrb, cxt, env, TRUE) + 1;
 
+  /*
+   * ci->env must be set prior to calling mrb_stack_extend() because
+   * it might reallocate the stack and envadjust() referes
+   * ci->env to rearrange each env->stack right address.
+   */
+  mrb->c->ci->env = env;
   mrb_stack_extend(mrb, slen, slen);
 
   cxt->syms = (mrb_sym *)mrb_realloc(mrb, cxt->syms, slen * sizeof(mrb_sym));
   cxt->slen = slen;
 
-  /* collect them */
-  __capture_lvs(mrb, cxt, env, false);
+  /* collect them in flatten envs */
+  __capture_lvs(mrb, cxt, env, FALSE);
 
   return slen;
 }
@@ -73,6 +107,7 @@ create_proc_from_string(mrb_state *mrb, char *s, int len, mrb_value binding, con
   struct mrb_parser_state *p;
   struct RProc *proc;
   struct mrb_context *c = mrb->c;
+  struct REnv *env = NULL;
 
   if (!mrb_nil_p(binding)) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "Binding of eval must be nil.");
@@ -90,7 +125,7 @@ create_proc_from_string(mrb_state *mrb, char *s, int len, mrb_value binding, con
   /* capture variables in envs */
   if (slen) {
     mrb_callinfo *prev_ci = &c->ci[-1];
-    struct REnv *env = prev_ci->proc->env;
+    env = prev_ci->proc->env;
 
     if (!MRB_PROC_CFUNC_P(prev_ci->proc)) {
       env = (struct REnv *)mrb_obj_alloc(mrb, MRB_TT_ENV, (struct RClass *)env);
@@ -98,7 +133,6 @@ create_proc_from_string(mrb_state *mrb, char *s, int len, mrb_value binding, con
       env->cioff = prev_ci - c->cibase;
       env->stack = c->ci->stackent;
       MRB_SET_ENV_STACK_LEN(env, prev_ci->proc->body.irep->nlocals);
-      c->ci->env = env;
     }
     *slen = capture_lvs(mrb, cxt, env);
   }
@@ -134,7 +168,8 @@ create_proc_from_string(mrb_state *mrb, char *s, int len, mrb_value binding, con
   if (c->ci[-1].proc->target_class) {
     proc->target_class = c->ci[-1].proc->target_class;
   }
-  proc->env = c->ci->env;
+
+  proc->env = env;
 
   return proc;
 }
@@ -150,16 +185,20 @@ f_eval(mrb_state *mrb, mrb_value self)
   mrb_value ret;
   struct RProc *proc;
   unsigned int keep = 0;
+  int ai = mrb_gc_arena_save(mrb);
 
   mrb_get_args(mrb, "s|ozi", &s, &len, &binding, &file, &line);
 
   proc = create_proc_from_string(mrb, s, len, binding, file, line, &keep);
+  mrb_gc_arena_restore(mrb, ai);
+  mrb_gc_protect(mrb, mrb_obj_value(proc));
+
   ret = mrb_top_run(mrb, proc, mrb->c->stack[0], keep);
   if (mrb->exc) {
     mrb_exc_raise(mrb, mrb_obj_value(mrb->exc));
   }
 
-  /* TODO: maybe need to writeback to env's stack from the stack */
+  writeback_lvs(mrb, proc);
 
   return ret;
 }
@@ -184,14 +223,15 @@ f_instance_eval(mrb_state *mrb, mrb_value self)
     mrb_int line = 1;
     mrb_value cv;
     struct RProc *proc;
+    unsigned int keep = 0;
 
     mrb_get_args(mrb, "s|zi", &s, &len, &file, &line);
     c->ci->acc = CI_ACC_SKIP;
     cv = mrb_singleton_class(mrb, self);
     c->ci->target_class = mrb_class_ptr(cv);
-    proc = create_proc_from_string(mrb, s, len, mrb_nil_value(), file, line, NULL);
+    proc = create_proc_from_string(mrb, s, len, mrb_nil_value(), file, line, &keep);
     mrb->c->ci->env = NULL;
-    return mrb_vm_run(mrb, proc, mrb->c->stack[0], 0);
+    return mrb_vm_run(mrb, proc, mrb->c->stack[0], keep);
   }
   else {
     mrb_get_args(mrb, "&", &b);
